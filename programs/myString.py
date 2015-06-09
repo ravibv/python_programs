8ac295f8f3aa7e351a0a8c7c9ba5e96f671eca6c
Ravi <ravi@localhost>
2015-06-09 11:36:15 +0530

    updated with float format specifiers

--- programs/myString.py
+++ programs/myString.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 
 
-first = "hi, %s", raw_input()
-second = "the date today is: %d", int(raw_input())
+first = "hi, %s" % raw_input("Enter name: ")
+second = "the date today is: %d" % int(raw_input("Enter number: "))
 
 print first, second
+
+########### PI ###########
+
+format  = "pi, with 3 decimals; %.3f" 
+
+import math
+
+print format % math.pi
